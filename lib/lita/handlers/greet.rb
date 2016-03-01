a0ba1c8a235aa156def44802383d8f23089103b1
Michael Chadwick <michael.chadwick@gmail.com>
2016-03-01 23:21:43 +0000

    added more routable greetings; bot now responds in kind; added tests for new functionality
    bot now greets with the same salutation; fixed tests
    
    bot response refactored; new tests that properly check new bot response

--- lib/lita/handlers/greet.rb
+++ lib/lita/handlers/greet.rb
@@ -2,21 +2,18 @@ module Lita
   module Handlers
     class Greet < Handler
 
-      route(/^(hello|hi!?(| .*)$|good morning|howdy|yo!?$|hey).*/i, :say_hello)
+      route(/^(ahoy|good (morning|afternoon|evening|night|nite)|greetings|hello|hey|hi!?(| .*)$|howdy|sup|yo!?$).*/i, :say_hello)
       route(/^welcome (.+)/i, :welcome)
 
       def say_hello(response)
         return if response.user.name.empty?
-        reply_to_name = response.user.metadata['mention_name'].nil? ?
-                         "#{response.user.name}" :
-                        "#{response.user.metadata['mention_name']}"
-        response.reply "Hello #{reply_to_name}!"
+        bot_response = response.message.body.gsub(/[?!]+/, "").gsub(/ #{robot.name}/i, "")
+        response.reply "#{bot_response} #{(response.user.mention_name || response.user.name)}"
       end
 
       def welcome(response)
         response.reply "Hello and Welcome #{response.matches[0][0]}!"
       end
-
 
     end
 
--- spec/lita/handlers/greet_spec.rb
+++ spec/lita/handlers/greet_spec.rb
@@ -4,14 +4,34 @@ describe Lita::Handlers::Greet, lita_handler: true do
   it { is_expected.to route("hello dog").to(:say_hello) }
   it { is_expected.to route("hi dog").to(:say_hello) }
   it { is_expected.to route("yo!").to(:say_hello) }
+  it { is_expected.to route("hey bot").to(:say_hello) }
+  it { is_expected.to route("greetings chatbot").to(:say_hello) }
   it { is_expected.to route("good morning").to(:say_hello) }
+  it { is_expected.to route("good afternoon").to(:say_hello) }
+  it { is_expected.to route("good evening").to(:say_hello) }
+  it { is_expected.to route("good night").to(:say_hello) }
+  it { is_expected.to route("good nite").to(:say_hello) }
   it { is_expected.to route("howdy").to(:say_hello) }
+  it { is_expected.to route("sup").to(:say_hello) }
+  it { is_expected.to route("ahoy").to(:say_hello) }
 
   it { is_expected.to route("welcome John Doe").to(:welcome) }
 
-  it "sends hello back the user who said hello" do
-    send_message("Hi")
+  it "sends hello back to the user who said hello" do
+    send_message("Hello")
     expect(replies.last).to match(/Hello/)
+  end
+
+  it "sends good morning back to the user name (since no mention_name is supplied) who greeted it" do
+    user = Lita::User.create("1", name: "Stephen")    
+    send_message("Good morning!", as: user)
+    expect(replies.last).to match(/Good morning Stephen/)
+  end
+
+  it "sends howdy back to the mention_name of the user who greeted it" do
+    user = Lita::User.create("2", name: "Robert", mention_name: "Bob")
+    send_message("Howdy", as: user)
+    expect(replies.last).to match(/Howdy Bob/)
   end
 
   it "does not send a message when the user object is empty" do
