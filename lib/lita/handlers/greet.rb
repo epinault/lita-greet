2f24a87124c9e3bb979e7053aac6872d3a9e448f
Michael Chadwick <michael.chadwick@gmail.com>
2016-03-01 23:21:43 +0000

    added some more trigger words, a more dynamic response from bot, and tests for them

--- lib/lita/handlers/greet.rb
+++ lib/lita/handlers/greet.rb
@@ -2,15 +2,16 @@ module Lita
   module Handlers
     class Greet < Handler
 
-      route(/^(hello|hi!?(| .*)$|good morning|howdy|yo!?$|hey).*/i, :say_hello)
+      route(/^(ahoy|greetings|good (morning|afternoon|evening|night|nite)|hello|hey|hi!?(| .*)$|howdy|sup|yo!?$).*/i, :say_hello)
       route(/^welcome (.+)/i, :welcome)
 
       def say_hello(response)
         return if response.user.name.empty?
         reply_to_name = response.user.metadata['mention_name'].nil? ?
-                         "#{response.user.name}" :
+                        "#{response.user.name}" :
                         "#{response.user.metadata['mention_name']}"
-        response.reply "Hello #{reply_to_name}!"
+        bot_greeting = ["Ahoy", "Greetings", "Hello", "Hey", "Hi", "Howdy", "Sup"].sample
+        response.reply "#{bot_greeting} #{reply_to_name}!"
       end
 
       def welcome(response)
--- spec/lita/handlers/greet_spec.rb
+++ spec/lita/handlers/greet_spec.rb
@@ -5,13 +5,18 @@ describe Lita::Handlers::Greet, lita_handler: true do
   it { is_expected.to route("hi dog").to(:say_hello) }
   it { is_expected.to route("yo!").to(:say_hello) }
   it { is_expected.to route("good morning").to(:say_hello) }
+  it { is_expected.to route("good afternoon").to(:say_hello) }
+  it { is_expected.to route("good evening").to(:say_hello) }
+  it { is_expected.to route("good night").to(:say_hello) }
   it { is_expected.to route("howdy").to(:say_hello) }
+  it { is_expected.to route("sup").to(:say_hello) }
+  it { is_expected.to route("ahoy").to(:say_hello) }
 
   it { is_expected.to route("welcome John Doe").to(:welcome) }
 
   it "sends hello back the user who said hello" do
     send_message("Hi")
-    expect(replies.last).to match(/Hello/)
+    expect(replies.last).to match(/^(ahoy|good (morning|afternoon|evening|night|nite)|hello|hey|hi!?(| .*)$|howdy|sup|yo!?$).*/i)
   end
 
   it "does not send a message when the user object is empty" do
